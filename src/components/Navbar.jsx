06e1e1b1fa44c0a9ee7a64ad362e23fca2011086
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2025-02-25 21:59:53 +0000

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-gray-700 py-4 px-6 shadow-lg">
+            <h1 className="text-white text-2xl font-bold">Writers&apos; Realm</h1>
+        </nav>
+    );
+}
